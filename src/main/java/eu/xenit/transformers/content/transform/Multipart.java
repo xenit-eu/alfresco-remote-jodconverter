2d9370bf302d5da2fdb32f9aebfded160609f79f
Roxana Angheluta <roxana.angheluta@xenit.eu>
2019-05-23 16:26:18 +0200

    JODCONV-1: correct javadoc

--- src/main/java/eu/xenit/transformers/content/transform/Multipart.java
+++ src/main/java/eu/xenit/transformers/content/transform/Multipart.java
@@ -13,17 +13,17 @@ import java.net.URLConnection;
 import org.alfresco.service.cmr.repository.ContentWriter;
 
 public class Multipart {
-        private final String boundary;
-        private static final String LINE_FEED = "\r\n";
-        private HttpURLConnection httpConn;
-        private String charset;
+    private final String boundary;
+    private static final String LINE_FEED = "\r\n";
+    private HttpURLConnection httpConn;
+    private String charset;
 
     public OutputStream getOutputStream() {
         return outputStream;
     }
 
     private OutputStream outputStream;
-        private PrintWriter writer;
+    private PrintWriter writer;
 
     public int getStatus() {
         return status;
@@ -31,48 +31,48 @@ public class Multipart {
 
     private int status;
 
-        /**
-         * This constructor initializes a new HTTP POST request with content type
-         * is set to multipart/form-data
-         *
-         * @param requestURL
-         * @param charset
-         * @throws IOException
-         */
-        public Multipart(String requestURL, String charset)
-                throws IOException {
-            this.charset = charset;
+    /**
+     * This constructor initializes a new HTTP POST request with content type
+     * is set to multipart/form-data
+     *
+     * @param requestURL
+     * @param charset
+     * @throws IOException
+     */
+    public Multipart(String requestURL, String charset)
+            throws IOException {
+        this.charset = charset;
 
-            // creates a unique boundary based on time stamp
-            boundary = "===" + System.currentTimeMillis() + "===";
-            URL url = new URL(requestURL);
-            httpConn = (HttpURLConnection) url.openConnection();
-            httpConn.setUseCaches(false);
-            httpConn.setDoOutput(true);    // indicates POST method
-            httpConn.setDoInput(true);
-            httpConn.setRequestProperty("Content-Type",
-                    "multipart/form-data; boundary=" + boundary);
-            outputStream = httpConn.getOutputStream();
-            writer = new PrintWriter(new OutputStreamWriter(outputStream, charset),
-                    true);
-        }
+        // creates a unique boundary based on time stamp
+        boundary = "===" + System.currentTimeMillis() + "===";
+        URL url = new URL(requestURL);
+        httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setUseCaches(false);
+        httpConn.setDoOutput(true);    // indicates POST method
+        httpConn.setDoInput(true);
+        httpConn.setRequestProperty("Content-Type",
+                "multipart/form-data; boundary=" + boundary);
+        outputStream = httpConn.getOutputStream();
+        writer = new PrintWriter(new OutputStreamWriter(outputStream, charset),
+                true);
+    }
 
-        /**
-         * Adds a form field to the request
-         *
-         * @param name  field name
-         * @param value field value
-         */
-        public void addFormField(String name, String value) {
-            writer.append("--" + boundary).append(LINE_FEED);
-            writer.append("Content-Disposition: form-data; name=\"" + name + "\"")
-                    .append(LINE_FEED);
-            writer.append("Content-Type: text/plain; charset=" + charset).append(
-                    LINE_FEED);
-            writer.append(LINE_FEED);
-            writer.append(value).append(LINE_FEED);
-            writer.flush();
-        }
+    /**
+     * Adds a form field to the request
+     *
+     * @param name  field name
+     * @param value field value
+     */
+    public void addFormField(String name, String value) {
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"")
+                .append(LINE_FEED);
+        writer.append("Content-Type: text/plain; charset=" + charset).append(
+                LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.append(value).append(LINE_FEED);
+        writer.flush();
+    }
 
     public void addFormFieldWithoutEnding(String name, String value) {
         writer.append("--" + boundary).append(LINE_FEED);
@@ -85,97 +85,93 @@ public class Multipart {
         writer.flush();
     }
 
-        /**
-         * Adds a upload file section to the request
-         *
-         * @param fieldName  name attribute in <input type="file" name="..." />
-         * @param uploadFile a File to be uploaded
-         * @throws IOException
-         */
-        public void addFilePart(String fieldName, File uploadFile)
-                throws IOException {
-            String fileName = uploadFile.getName();
-            writer.append("--" + boundary).append(LINE_FEED);
-            writer.append(
-                    "Content-Disposition: form-data; name=\"" + fieldName
-                            + "\"; filename=\"" + fileName + "\"")
-                    .append(LINE_FEED);
-            writer.append(
-                    "Content-Type: "
-                            + URLConnection.guessContentTypeFromName(fileName))
-                    .append(LINE_FEED);
-            writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
-            writer.append(LINE_FEED);
-            writer.flush();
+    /**
+     * Adds a upload file section to the request
+     *
+     * @param fieldName  name attribute in input type="file" name="..."
+     * @param uploadFile a File to be uploaded
+     * @throws IOException
+     */
+    public void addFilePart(String fieldName, File uploadFile)
+            throws IOException {
+        String fileName = uploadFile.getName();
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append(
+                "Content-Disposition: form-data; name=\"" + fieldName
+                        + "\"; filename=\"" + fileName + "\"")
+                .append(LINE_FEED);
+        writer.append(
+                "Content-Type: "
+                        + URLConnection.guessContentTypeFromName(fileName))
+                .append(LINE_FEED);
+        writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.flush();
 
-            FileInputStream inputStream = new FileInputStream(uploadFile);
-            byte[] buffer = new byte[4096];
-            int bytesRead = -1;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            outputStream.flush();
-            inputStream.close();
-            writer.append(LINE_FEED);
-            writer.flush();
+        FileInputStream inputStream = new FileInputStream(uploadFile);
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
         }
+        outputStream.flush();
+        inputStream.close();
+        writer.append(LINE_FEED);
+        writer.flush();
+    }
 
-        public void addInputStreamPart(String fieldName, InputStream inputStream, String mimetype)
-                throws IOException {
-            String fileName = "dummy.docx";
-            writer.append("--" + boundary).append(LINE_FEED);
-            writer.append(
-                    "Content-Disposition: form-data; name=\"" + fieldName
-                            + "\"; filename=\"" + fileName + "\"")
-                    .append(LINE_FEED);
-            writer.append(
-                    "Content-Type: "
-                            + mimetype)
-                    .append(LINE_FEED);
-            writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
-            writer.append(LINE_FEED);
-            writer.flush();
+    public void addInputStreamPart(String fieldName, InputStream inputStream, String mimetype)
+            throws IOException {
+        String fileName = "dummy.docx";
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append(
+                "Content-Disposition: form-data; name=\"" + fieldName
+                        + "\"; filename=\"" + fileName + "\"")
+                .append(LINE_FEED);
+        writer.append(
+                "Content-Type: "
+                        + mimetype)
+                .append(LINE_FEED);
+        writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.flush();
 
-            byte[] buffer = new byte[4096];
-            int bytesRead = -1;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            outputStream.flush();
-            writer.append(LINE_FEED);
-            writer.flush();
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
         }
+        outputStream.flush();
+        writer.append(LINE_FEED);
+        writer.flush();
+    }
 
-        /**
-         * Adds a header field to the request.
-         *
-         * @param name  - name of the header field
-         * @param value - value of the header field
-         */
-        public void addHeaderField(String name, String value) {
-            writer.append(name + ": " + value).append(LINE_FEED);
-            writer.flush();
-        }
+    /**
+     * Adds a header field to the request.
+     *
+     * @param name  - name of the header field
+     * @param value - value of the header field
+     */
+    public void addHeaderField(String name, String value) {
+        writer.append(name + ": " + value).append(LINE_FEED);
+        writer.flush();
+    }
 
-        /**
-         * Completes the request and receives response from the server.
-         *
-         * @return a list of Strings as response in case the server returned
-         * status OK, otherwise an exception is thrown.
-         * @throws IOException
-         */
-        public void finish(ContentWriter finalWriter) throws IOException {
-            writer.append(LINE_FEED).flush();
-            writer.append("--" + boundary + "--").append(LINE_FEED);
-            writer.close();
+    /**
+     * Completes the request and receives response from the server.
+     */
+    public void finish(ContentWriter finalWriter) throws IOException {
+        writer.append(LINE_FEED).flush();
+        writer.append("--" + boundary + "--").append(LINE_FEED);
+        writer.close();
 
-            // checks server's status code first
-            status = httpConn.getResponseCode();
-            if (status == HttpURLConnection.HTTP_OK) {
-                finalWriter.putContent(httpConn.getInputStream());
-                httpConn.disconnect();
-            } else {
-                throw new IOException("Server returned non-OK status: " + status);
-            }
+        // checks server's status code first
+        status = httpConn.getResponseCode();
+        if (status == HttpURLConnection.HTTP_OK) {
+            finalWriter.putContent(httpConn.getInputStream());
+            httpConn.disconnect();
+        } else {
+            throw new IOException("Server returned non-OK status: " + status);
         }
     }
+}
